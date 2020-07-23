061b24a0e36e5e9e5f32dde06b685d957b629811
babanazar <g.babanazar@gmail.com>
2020-07-24 01:15:09 +0300

    db for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "18.216.113.119", user: "deploy", roles: %w{app web}
+server "18.216.113.119", user: "deploy", roles: %w{app db web}
 
 
 # role-based syntax
